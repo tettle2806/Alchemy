f6fdd2ea224ab93af413c2e563290e6b9a8e6c48
Zayniddin <126386572+tettle2806@users.noreply.github.com>
2025-05-28 17:31:14 +0500

    Crud методы основанные на классах

--- dao/base.py
+++ dao/base.py
@@ -1,15 +1,14 @@
-from typing import List, Dict, Any
-
-from sqlalchemy.ext.asyncio import AsyncSession
+from typing import List, Any, Dict
 from sqlalchemy.exc import SQLAlchemyError
+from sqlalchemy.ext.asyncio import AsyncSession
 
 
-class BaseDao:
-    model = None
+class BaseDAO:
+    model = None  # Устанавливается в дочернем классе
 
     @classmethod
     async def add(cls, session: AsyncSession, **values):
-
+        # Добавить одну запись
         new_instance = cls.model(**values)
         session.add(new_instance)
         try:
@@ -21,11 +20,11 @@ class BaseDao:
 
     @classmethod
     async def add_many(cls, session: AsyncSession, instances: List[Dict[str, Any]]):
-        new_instance = [cls.model(**values) for values in instances]
-        session.add_all(new_instance)
+        new_instances = [cls.model(**values) for values in instances]
+        session.add_all(new_instances)
         try:
             await session.commit()
         except SQLAlchemyError as e:
             await session.rollback()
             raise e
-        return new_instance
+        return new_instances
