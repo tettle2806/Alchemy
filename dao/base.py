9f0058f1c03482a47f73e271e3905f8b4b3c4106
Zayniddin <126386572+tettle2806@users.noreply.github.com>
2025-05-28 18:01:37 +0500

    Создание пользователя и профиля

--- dao/base.py
+++ dao/base.py
@@ -28,3 +28,36 @@ class BaseDAO:
             await session.rollback()
             raise e
         return new_instances
+
+
+class UserDao(BaseDAO):
+    model = User
+
+    @classmethod
+    async def add_user_with_profile(cls, session:AsyncSession, user_data: dict) -> User:
+        user = cls.model(
+            username=user_data['username'],
+            email= user_data['email'],
+            password=user_data['password'],
+        )
+        session.add(user)
+        await session.flush()
+
+        profile = Profile(
+            user_id=user.id,
+            first_name=user_data['first_name'],
+            last_name=user_data.get('last_name'),
+            age=user_data.get('age'),
+            gender=user_data['gender'],
+            profession=user_data.get('profession'),
+            interests=user_data.get('interests'),
+            contacts=user_data.get('contacts')
+        )
+        session.add(profile)
+
+        await session.commit()
+
+        return user
+
+
+
