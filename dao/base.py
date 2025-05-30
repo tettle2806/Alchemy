f8d56c31c3b186189b5f23f0587a8e397582f080
Zayniddin <126386572+tettle2806@users.noreply.github.com>
2025-05-30 17:30:07 +0500

    Универсальный метод для получения одной записи по нескольким параметрам

--- dao/base.py
+++ dao/base.py
@@ -40,3 +40,10 @@ class BaseDAO:
         result = await session.execute(query)
         record = result.scalar_one_or_none()
         return record
+
+    @classmethod
+    async def find_one_or_none(cls, session:AsyncSession, **filter_by):
+        query = select(cls.model).filter_by(**filter_by)
+        result = await session.execute(query)
+        record = result.scalar_one_or_none()
+        return record
